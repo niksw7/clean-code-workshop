b5c474675ef710278da9d15bab420e2960e1c4db
bala.nallamilli <bala.nallamilli@pragmaticplay.com>
2022-11-05 17:05:43 +0530

    refactored code

--- main.go
+++ main.go
@@ -5,11 +5,17 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"path"
 	"strconv"
 	"sync/atomic"
 )
+
+var ErrorLog = log.New(os.Stdout, "Error", log.LstdFlags)
+var InfoLog = log.New(os.Stdout, "Info", log.LstdFlags)
+var DebugLog = log.New(os.Stdout, "Debug", log.LstdFlags)
+var WarnLog = log.New(os.Stdout, "Warn", log.LstdFlags)
 
 const (
 	Base     = 10
@@ -19,31 +25,85 @@ const (
 	TeraByte = 1000 * GigaByte
 )
 
-func traverseDir(hashes, duplicates map[string]string, dupeSize *int64, entries []os.FileInfo, directory string) {
-	for _, entry := range entries {
-		fullpath := path.Join(directory, entry.Name())
-		if !entry.Mode().IsDir() && !entry.Mode().IsRegular() {
-			continue
-		}
-		if entry.IsDir() {
-			dirFiles, err := ioutil.ReadDir(fullpath)
-			if err != nil {
-				panic(err)
-			}
-			traverseDir(hashes, duplicates, dupeSize, dirFiles, fullpath)
-			continue
-		}
-		hashString, hashErr := generateSHA1(fullpath)
-		if hashErr != nil {
-			panic(hashErr)
-		}
-		if hashEntry, ok := hashes[hashString]; ok {
-			duplicates[hashEntry] = fullpath
-			atomic.AddInt64(dupeSize, entry.Size())
-		} else {
-			hashes[hashString] = fullpath
+type NilEntry struct{}
+
+func (n NilEntry) Handle(information *DuplicatesInformation) error {
+	return nil
+}
+
+type EntryHandler interface {
+	Handle(information *DuplicatesInformation) error
+}
+
+type DirEntry struct {
+	Fullpath string
+}
+
+func (d DirEntry) Handle(information *DuplicatesInformation) error {
+	dirFiles, err := ioutil.ReadDir(d.Fullpath)
+	if err != nil {
+		ErrorLog.Println(err)
+		return err
+	}
+	return traverseDir(information, dirFiles, d.Fullpath)
+}
+
+type FileEntry struct {
+	FullPath string
+	Size     int64
+}
+
+func (f FileEntry) Handle(information *DuplicatesInformation) error {
+	hashString, hashErr := generateSHA1(f.FullPath)
+	if hashErr != nil {
+		ErrorLog.Println(hashErr)
+		return hashErr
+	}
+	information.AddDuplicates(hashString, f.FullPath, f.Size)
+	return nil
+}
+
+func NewEntryHandler(entry os.FileInfo, directory string) EntryHandler {
+	fullPath := path.Join(directory, entry.Name())
+	if entry.Mode().IsDir() {
+		return &DirEntry{
+			Fullpath: fullPath,
 		}
 	}
+	if entry.Mode().IsRegular() {
+		return &FileEntry{
+			fullPath,
+			entry.Size(),
+		}
+	}
+	return NilEntry{}
+}
+
+type DuplicatesInformation struct {
+	Hashes     map[string]string
+	Duplicates map[string]string
+	DupeSize   *int64
+}
+
+func (d *DuplicatesInformation) AddDuplicates(hashString, fullPath string, size int64) {
+	if hashEntry, ok := d.Hashes[hashString]; ok {
+		d.Duplicates[hashEntry] = fullPath
+		atomic.AddInt64(d.DupeSize, size)
+	} else {
+		d.Hashes[hashString] = fullPath
+	}
+
+}
+
+func traverseDir(d *DuplicatesInformation, entries []os.FileInfo, directory string) error {
+	for _, entry := range entries {
+		entryHandler := NewEntryHandler(entry, directory)
+		err := entryHandler.Handle(d)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func generateSHA1(filePath string) (string, error) {
@@ -76,33 +136,38 @@ func toReadableSize(nbytes int64) string {
 }
 
 func main() {
+
 	var err error
-	dir := flag.String("path", "", "the path to traverse searching for duplicates")
+	dir := flag.String("path", "./fixtures", "the path to traverse searching for duplicates")
 	flag.Parse()
 
 	if *dir == "" {
 		*dir, err = os.Getwd()
 		if err != nil {
+			ErrorLog.Println(err)
 			panic(err)
 		}
 	}
 
-	hashes := map[string]string{}
-	duplicates := map[string]string{}
-	var dupeSize int64
-
 	entries, err := ioutil.ReadDir(*dir)
 	if err != nil {
+		ErrorLog.Println(err)
 		panic(err)
 	}
-
-	traverseDir(hashes, duplicates, &dupeSize, entries, *dir)
-
+	d := DuplicatesInformation{
+		map[string]string{},
+		map[string]string{},
+		new(int64),
+	}
+	err = traverseDir(&d, entries, *dir)
+	if err != nil {
+		ErrorLog.Println(err)
+		panic(err)
+	}
 	fmt.Println("DUPLICATES")
-
-	fmt.Println("TOTAL FILES:", len(hashes))
-	fmt.Println("DUPLICATES:", len(duplicates))
-	fmt.Println("TOTAL DUPLICATE SIZE:", toReadableSize(dupeSize))
+	fmt.Println("TOTAL UNIQUE FILES:", len(d.Hashes))
+	fmt.Println("DUPLICATES:", len(d.Duplicates))
+	fmt.Println("TOTAL DUPLICATE SIZE:", toReadableSize(*d.DupeSize))
 }
 
 // running into problems of not being able to open directories inside .app folders
--- main_test.go
+++ main_test.go
@@ -7,49 +7,6 @@ import (
 	"testing"
 )
 
-func Test_traverseDir(t *testing.T) {
-	entry, _ := ioutil.ReadDir("./fixtures")
-	type args struct {
-		hashes     map[string]string
-		duplicates map[string]string
-		dupeSize   *int64
-		entries    []os.FileInfo
-		directory  string
-	}
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "testSuccessPath",
-			args: args{
-				hashes:     make(map[string]string),
-				duplicates: make(map[string]string),
-				dupeSize:   new(int64),
-				entries:    make([]os.FileInfo, 0),
-				directory:  ".",
-			},
-		},
-		{
-			name: "test1",
-			args: args{
-				hashes:     make(map[string]string),
-				duplicates: make(map[string]string),
-				dupeSize:   new(int64),
-				entries:    entry,
-				directory:  "./fixtures",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			traverseDir(tt.args.hashes, tt.args.duplicates, tt.args.dupeSize, tt.args.entries, tt.args.directory)
-			t.Log("totalDuplicates", *tt.args.dupeSize)
-			fmt.Println(tt.name, " -->> ", *tt.args.dupeSize, tt.args.directory)
-		})
-	}
-}
-
 func Test_toReadableSize(t *testing.T) {
 	type args struct {
 		nbytes int64
@@ -100,6 +57,46 @@ func Test_toReadableSize(t *testing.T) {
 			if got := toReadableSize(tt.args.nbytes); got != tt.want {
 				t.Errorf("toReadableSize() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_traverseDir1(t *testing.T) {
+	entry, _ := ioutil.ReadDir("./fixtures")
+	type args struct {
+		d         DuplicatesInformation
+		entries   []os.FileInfo
+		directory string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test",
+			args: args{
+				d:         DuplicatesInformation{make(map[string]string), make(map[string]string), new(int64)},
+				entries:   make([]os.FileInfo, 0),
+				directory: ".",
+			},
+		},
+		{
+			name: "test2",
+			args: args{
+				d:         DuplicatesInformation{make(map[string]string), make(map[string]string), new(int64)},
+				entries:   entry,
+				directory: "./fixtures",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := traverseDir(&tt.args.d, tt.args.entries, tt.args.directory); (err != nil) != tt.wantErr {
+				t.Errorf("traverseDir() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			fmt.Println(tt.name, " -->> ", *tt.args.d.DupeSize, tt.args.directory)
 		})
 	}
 }
