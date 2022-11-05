efee89dd3a3cc4058f1e4701839b02e93227011c
bala.nallamilli <bala.nallamilli@pragmaticplay.com>
2022-11-05 14:02:31 +0530

    added mores tests for helper functions

--- main_test.go
+++ main_test.go
@@ -44,8 +44,62 @@ func Test_traverseDir(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			traverseDir(tt.args.hashes, tt.args.duplicates, tt.args.dupeSize, tt.args.entries, tt.args.directory)
-			t.Log("totalDuplicates", tt.args.dupeSize)
+			t.Log("totalDuplicates", *tt.args.dupeSize)
 			fmt.Println(tt.name, " -->> ", *tt.args.dupeSize, tt.args.directory)
+		})
+	}
+}
+
+func Test_toReadableSize(t *testing.T) {
+	type args struct {
+		nbytes int64
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "1000 Bytes",
+			args: args{
+				nbytes: 1000,
+			},
+			want: "1000 B",
+		},
+		{
+			name: "1000 KB",
+			args: args{
+				nbytes: 1000000,
+			},
+			want: "1000 KB",
+		},
+		{
+			name: "1000 MB",
+			args: args{
+				nbytes: 1000000000,
+			},
+			want: "1000 MB",
+		},
+		{
+			name: "1000 GB",
+			args: args{
+				nbytes: 1000000000000,
+			},
+			want: "1000 GB",
+		},
+		{
+			name: "1000 TB",
+			args: args{
+				nbytes: 1000000000000000,
+			},
+			want: "1000 TB",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toReadableSize(tt.args.nbytes); got != tt.want {
+				t.Errorf("toReadableSize() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
